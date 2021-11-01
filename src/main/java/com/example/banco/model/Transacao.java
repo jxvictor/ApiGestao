b159aebffbafa1cb50251ecfb5b3356c9d1ccac9
João Victor Costa Alves <57672775+jxvictor@users.noreply.github.com>
2021-11-01 10:20:04 -0300

    Update Transacao.java

--- src/main/java/com/example/banco/model/Transacao.java
+++ src/main/java/com/example/banco/model/Transacao.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -30,15 +31,19 @@ public class Transacao implements Serializable{
 	private static final long serialVersionUID = 4937435397910368171L;
 	
 	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	@ApiModelProperty(value = "Identificador único da transação")
 	private long id;
 	
 	@ManyToOne(optional = true)
+	@ApiModelProperty(value = "Identificador único da Conta")
 	private Conta conta;
 	
 	@Column(nullable = false)
+	@ApiModelProperty(value = "Digite o valor da transação")
 	private double valor;
 	
 	@JsonFormat(pattern="dd/MM/yyyy")
 	@Column(name = "dataTransacao")
+	@ApiModelProperty(value = "Digite a data da transação")
 	private Date dataTransacao;
 }
