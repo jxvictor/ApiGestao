f2329179734afc4ac4c164b74d86f00efdbaeacc
João Victor Costa Alves <57672775+jxvictor@users.noreply.github.com>
2021-11-01 10:19:35 -0300

    Update Conta.java

--- src/main/java/com/example/banco/model/Conta.java
+++ src/main/java/com/example/banco/model/Conta.java
@@ -18,6 +18,7 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -36,29 +37,37 @@ public class Conta implements Serializable {
 
 	@Id @GeneratedValue(strategy = GenerationType.AUTO)
 	@Column()
+	@ApiModelProperty(value = "Identificador único para a Conta")
 	private long id;
 	
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "id_Pessoa")
+	@ApiModelProperty(value = "Identificador único para Pessoa")
 	private Pessoa pessoa;
 	
 	@JsonIgnoreProperties
 	@OneToMany(mappedBy="conta")
 	private List<Transacao> transacoes;
 
+	
 	@Column(name = "saldo", nullable = false)
+	@ApiModelProperty(value = "Digite o saldo da conta")
 	private double saldo;
 	
 	@Column(nullable = false)
+	@ApiModelProperty(value = "Digite o Limite de saque diário da conta")
 	private double limiteSaqueDiario;
 	
 	@Column(nullable = false)
+	@ApiModelProperty(value = "Digite se se o Flag está ativo(true) ou desativada(false)")
 	private boolean flagAtivo;
 	
 	@Column(nullable = false)
+	@ApiModelProperty(value = "Digite o Tipo da conta")
 	private int tipoConta;
 	
 	@Column(name = "dataCriacao", nullable = false)
 	@JsonFormat(pattern="dd/MM/yyyy")
+	@ApiModelProperty(value = "Digite a Data de criação da conta")
 	private Date dataCriacao;
 }
